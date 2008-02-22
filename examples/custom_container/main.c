fabce6190f307bd83d7e124ec3aa7d685b3b4a4d
Murray Cumming <murrayc@murrayc.com>
2008-02-22 19:32:57 +0000

    2008-02-22  Murray Cumming  <murrayc@murrayc.com>
    * configure.ac:
    * docs/tutorial/clutter-tut.xml:
    * docs/tutorial/figures/Makefile.am:
    * examples/Makefile.am:
    * examples/actor_group/: Added actors/containers sub-section, 
    mentioning ClutterGroup.
    
    * examples/custom_container/: Added placeholder for a custom 
    containers section and example.

---
+++ examples/custom_container/main.c
@@ -0,0 +1,49 @@
+/* Copyright 2007 Openismus GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include <clutter/clutter.h>
+#include "triangle_actor.h"
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+  ClutterColor stage_color = { 0x00, 0x00, 0x00, 0xff };
+  ClutterColor actor_color = { 0xff, 0xff, 0xff, 0x99 };
+
+  clutter_init (&argc, &argv);
+
+  /* Get the stage and set its size and color: */
+  ClutterActor *stage = clutter_stage_get_default ();
+  clutter_actor_set_size (stage, 200, 200);
+  clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
+
+  /* Add our custom actor to the stage: */
+  ClutterActor *actor = clutter_triangle_new_with_color (&actor_color);
+  clutter_actor_set_size (actor, 100, 100);
+  clutter_actor_set_position (actor, 20, 20);
+  clutter_container_add_actor (CLUTTER_CONTAINER (stage), actor);
+  clutter_actor_show (actor);
+
+  /* Show the stage: */
+  clutter_actor_show (stage);
+
+  /* Start the main loop, so we can respond to events: */
+  clutter_main ();
+
+  return EXIT_SUCCESS;
+
+}
