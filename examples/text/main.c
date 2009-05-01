3786aa70cd2a7dd79966c25262a22ca1871d8a01
Murray Cumming <murrayc@murrayc.com>
2009-05-01 10:13:48 +0200

    ClutterText corrections.
    * docs/tutorial/clutter-tut.xml: ClutterText section: Correct/Improve the
    text.
    * examples/text/main.c (main): Add a second ClutterText so we have one
    non-editable and one editable multi-line editable.

--- examples/text/main.c
+++ examples/text/main.c
@@ -17,8 +17,6 @@
 #include <clutter/clutter.h>
 #include <stdlib.h>
 
-ClutterActor *text = NULL;
-
 int main(int argc, char *argv[])
 {
   ClutterColor stage_color = { 0x00, 0x00, 0x00, 0xff };
@@ -31,24 +29,49 @@ int main(int argc, char *argv[])
   clutter_actor_set_size (stage, 600, 200);
   clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
 
-  /* Add a text actor to the stage: */
-  text = clutter_text_new ();
+
+  /* Add a non-editable text actor to the stage: */
+  ClutterActor *text = clutter_text_new ();
 
 	/* Setup text properties */
   clutter_text_set_color (CLUTTER_TEXT (text), &actor_color);
   clutter_text_set_text (CLUTTER_TEXT (text), 
-    "Wizard imps and sweat sock pimps, interstellar mongrel nymphs.");
+    "Non-editable text: Wizard imps and sweat sock pimps, interstellar mongrel nymphs.");
   clutter_text_set_font_name (CLUTTER_TEXT (text), "Sans 12");
-	clutter_text_set_editable (CLUTTER_TEXT (text), TRUE);
-	clutter_text_set_line_wrap (CLUTTER_TEXT (text), TRUE);
+	clutter_text_set_editable (CLUTTER_TEXT (text), FALSE);
+	clutter_text_set_line_wrap (CLUTTER_TEXT (text), FALSE);
 
   clutter_actor_set_size (text, 590, 100);
   clutter_actor_set_position (text, 5, 5);
 	clutter_container_add_actor (CLUTTER_CONTAINER (stage), text);
   clutter_actor_show (text);
+  
+  
+   /* Add a multi-line editable text actor to the stage: */
+  text = clutter_text_new ();
+
+	/* Setup text properties */
+  clutter_text_set_color (CLUTTER_TEXT (text), &actor_color);
+  clutter_text_set_text (CLUTTER_TEXT (text), 
+    "Editable text: And as I sat there brooding on the old, unknown world, I thought of "
+     "Gatsby's wonder when he first picked out the green light at the end of "
+     "Daisy's dock. He had come a long way to this blue lawn and his dream "
+     "must have seemed so close that he could hardly fail to grasp it. He did "
+     "not know that it was already behind him, somewhere back in that vast "
+     "obscurity beyond the city, where the dark fields of the republic rolled "
+     "on under the night.");
+  clutter_text_set_font_name (CLUTTER_TEXT (text), "Sans 12");
+	clutter_text_set_editable (CLUTTER_TEXT (text), TRUE);
+	clutter_text_set_line_wrap (CLUTTER_TEXT (text), TRUE);
+
+  //clutter_actor_set_size (text, 590, 100);
+  clutter_actor_set_position (text, 5, 30);
+	clutter_container_add_actor (CLUTTER_CONTAINER (stage), text);
+  clutter_actor_show (text);
 
   /* Set focus to handle key presses on the stage: */
   clutter_stage_set_key_focus (CLUTTER_STAGE (stage), text);
+  
 
   /* Show the stage: */
   clutter_actor_show (stage);
