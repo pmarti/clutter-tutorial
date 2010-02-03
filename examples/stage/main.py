de6fe807b7b7bf166005b465ea685c4da03d1cf1
Pablo Martí <pmarti@warp.es>
2010-02-03 13:35:25 +0100

    stage example translated

---
+++ examples/stage/main.py
@@ -0,0 +1,25 @@
+import sys
+
+import clutter
+
+def on_stage_button_press(stage, event):
+    print "Stage clicked at (%d, %d)" % (event.x, event.y)
+
+
+def main():
+    stage = clutter.Stage()
+
+    stage = clutter.Stage()
+    stage.set_size(200, 200)
+    stage.set_color(clutter.Color(0, 0, 0, 255))
+
+    stage.connect('button-press-event', on_stage_button_press)
+    stage.connect("destroy", clutter.main_quit)
+    stage.show()
+
+    clutter.main()
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
